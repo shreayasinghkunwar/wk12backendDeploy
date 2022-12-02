37235035e66aae0aa2ea85d70d6ef2c703031201
shreayasinghkunwar <shreaya@fusemachines.com>
2022-12-02 14:37:42 +0545

    removing cor

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ connection.once("open", function () {
 
 app.use(express.json());
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-    origin: ["http://localhost:3000", "https://contact-app-ssk.onrender.com"],
-}));
+app.use(cors());
 app.use(router);
 const port = 8000;
 
